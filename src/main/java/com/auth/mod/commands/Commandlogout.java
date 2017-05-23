deea948738d803057becd8250a8cbfc7859b6d0a
Johncjx <Johncjx@users.noreply.github.com>
2017-05-23 16:57:26 +0800

    add permission check
    add permission check

--- src/main/java/com/auth/mod/commands/Commandlogout.java
+++ src/main/java/com/auth/mod/commands/Commandlogout.java
@@ -4,13 +4,14 @@ import net.minecraft.util.text.TextFormatting;
 
 import net.minecraft.command.CommandBase;
 import net.minecraft.command.CommandException;
+import net.minecraft.command.ICommand;
 import net.minecraft.command.ICommandSender;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.server.MinecraftServer;
 import net.minecraft.util.text.TextComponentString;
 
 
-public class Commandlogout extends CommandBase {
+public class Commandlogout extends CommandBase implements ICommand {
 	
 	@Override
 	public String getCommandName()
@@ -24,6 +25,12 @@ public class Commandlogout extends CommandBase {
 	return "/logout";
 	}
 
+	@Override
+	public boolean checkPermission(MinecraftServer server, ICommandSender sender)
+	{
+		return true;
+	}
+	
 	@Override
 	public void execute(MinecraftServer server, ICommandSender sender, String[] args) throws CommandException {
 		EntityPlayer player = (EntityPlayer) sender;
