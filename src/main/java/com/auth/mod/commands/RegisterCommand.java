1f69d08dff837ec75f4891d23bb929ad4596bcb8
Johncjx <Johncjx@users.noreply.github.com>
2017-05-23 16:59:14 +0800

    add permission check
    add permission check

--- src/main/java/com/auth/mod/commands/RegisterCommand.java
+++ src/main/java/com/auth/mod/commands/RegisterCommand.java
@@ -11,13 +11,14 @@ import net.minecraft.util.text.TextFormatting;
 
 import net.minecraft.command.CommandBase;
 import net.minecraft.command.CommandException;
+import net.minecraft.command.ICommand;
 import net.minecraft.command.ICommandSender;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.server.MinecraftServer;
 import net.minecraft.util.text.TextComponentString;
 
 
-public class RegisterCommand extends CommandBase {
+public class RegisterCommand extends CommandBase implements ICommand {
 	
 	@Override
 	public String getCommandName()
@@ -29,6 +30,12 @@ public class RegisterCommand extends CommandBase {
 	public String getCommandUsage(ICommandSender icommandsender)
 	{
 	return "/register <password>";	}
+	
+	@Override
+	public boolean checkPermission(MinecraftServer server, ICommandSender sender)
+	{
+		return true;
+	}
 
 	@Override
 	public void execute(MinecraftServer server, ICommandSender sender, String[] args) throws CommandException {
