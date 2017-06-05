252a9db6efafd0b2b726e8f8593f6134808da779
Fungie2134 <qwerty15@gmx.fr>
2017-06-05 19:21:50 +0200

    Fixed give bug

--- src/main/java/com/auth/mod/events/DropItem.java
+++ src/main/java/com/auth/mod/events/DropItem.java
@@ -3,17 +3,21 @@ package com.auth.mod.events;
 import com.auth.mod.Main;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.item.ItemStack;
+import net.minecraft.server.MinecraftServer;
+import net.minecraftforge.event.entity.item.ItemEvent;
 import net.minecraftforge.event.entity.item.ItemTossEvent;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
+import net.minecraftforge.fml.server.FMLServerHandler;
 
 public class DropItem {
 	@SubscribeEvent
 	  public void DropItems(ItemTossEvent evt) {
 			
-			if(!Main.logged.contains(evt.getPlayer().getName())){
+			if(!Main.logged.contains(evt.getPlayer().getName()) && Main.passwords.containsKey(evt.getPlayer().getName())){
+				
 			ItemStack itemStack = evt.getEntityItem().getEntityItem();
-			evt.setCanceled(true);
-			evt.getPlayer().inventory.addItemStackToInventory(itemStack);
+            evt.setCanceled(true);
+            evt.getPlayer().inventory.addItemStackToInventory(itemStack);
 			}
 			
 		
